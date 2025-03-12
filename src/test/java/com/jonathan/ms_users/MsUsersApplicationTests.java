3ac7819df69bb45d2190a1298a1780d5c9dd2cdf
Jonathan <jadougdev@gmail.com>
2025-03-12 05:22:31 -0500

    Added app properties per profile

--- src/test/java/com/jonathan/ms_users/MsUsersApplicationTests.java
+++ src/test/java/com/jonathan/ms_users/MsUsersApplicationTests.java
@@ -2,8 +2,10 @@ package com.jonathan.ms_users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsUsersApplicationTests {
 
 	@Test
